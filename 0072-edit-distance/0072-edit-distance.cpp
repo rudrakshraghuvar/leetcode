789d4d29fda224b039bca0e9e152008832d5ac32
Rudra Pratap Singh <60797245+rudrakshraghuvar@users.noreply.github.com>
2023-02-26 12:39:15 +0530

    Time: 176 ms (6.47%), Space: 80.5 MB (14.57%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -9,7 +9,7 @@ public:
         if(dp[i][j] != -1) return dp[i][j];
         
         if(w1[i] == w2[j]) 
-            return solve(i-1, j-1, w1, w2);
+            return dp[i][j] = solve(i-1, j-1, w1, w2);
         int del = 1 + solve(i-1, j, w1, w2); // delete
         int rep = 1 + solve(i-1, j-1, w1, w2); // replace
         int ins = 1 + solve(i, j-1, w1, w2); // insert
