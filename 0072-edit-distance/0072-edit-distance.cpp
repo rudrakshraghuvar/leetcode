72c226045e1a5159f56b3c463f505ae9b8c46a3f
Rudra Pratap Singh <60797245+rudrakshraghuvar@users.noreply.github.com>
2023-02-26 12:55:35 +0530

    Time: 6 ms (93.28%), Space: 7.3 MB (79.78%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -1,26 +1,23 @@
 class Solution {
 public:
-    int dp[501][501];
-    
-    int solve(int i, int j, string w1, string w2) {
-        if(i < 0) return j+1;
-        if(j < 0) return i+1;
-        
-        if(dp[i][j] != -1) return dp[i][j];
-        
-        if(w1[i] == w2[j]) 
-            return dp[i][j] = solve(i-1, j-1, w1, w2);
-        int del = 1 + solve(i-1, j, w1, w2); // delete
-        int rep = 1 + solve(i-1, j-1, w1, w2); // replace
-        int ins = 1 + solve(i, j-1, w1, w2); // insert
-        return dp[i][j] = min(del, min(rep, ins));
-    }
-    
     int minDistance(string w1, string w2) {
         int n1 = w1.size(), n2 = w2.size();
+        int dp[n1+1][n2+1];
         
-        memset(dp, -1, sizeof(dp));
+        for(int i = 0; i <= n1; i++)
+            dp[i][0] = i;
+        for(int j = 0; j <= n2; j++)
+            dp[0][j] = j;
         
-        return solve(n1-1, n2-1, w1, w2);
+        for(int i = 1; i <= n1; i++) {
+            for(int j = 1; j <= n2; j++) {
+                if(w1[i-1] == w2[j-1])
+                    dp[i][j] = dp[i-1][j-1];
+                else
+                    dp[i][j] = 1 + min(dp[i-1][j], min(dp[i][j-1], dp[i-1][j-1]));
+            }
+        }
+        
+        return dp[n1][n2];
     }
 };
